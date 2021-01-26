4fc26cf748998def3073a20efe1b6a8517fcfd8c
wenyuan.gu <wenyuan.gu@petuum.com>
2021-01-26 14:08:22 -0800

    change on_train_begin to on epoch begin

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ class SetAutoDistSession(keras.callbacks.Callback):
         self.autodist = autodist
         self.sess = None
 
-    def on_train_begin(self, epoch, logs=None):
+    def on_epoch_begin(self, epoch, logs=None):
         if not self.sess:
             self.sess = self.autodist.create_distributed_session()
             keras.backend.set_session(self.sess)
